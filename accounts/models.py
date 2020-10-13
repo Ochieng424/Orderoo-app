b01a0840370b6c85a704b386a2423ace3295acc6
Ochieng424 <ochiengderrick424@gmail.com>
2020-10-13 19:39:54 +0300

    modal queries, rendering data to template(template tags)

--- accounts/models.py
+++ accounts/models.py
@@ -44,5 +44,5 @@ class Order(models.Model):
     )
     customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
-    status = models.CharField(max_length=25, null=True)
-    date_created = models.DateTimeField(auto_now_add=True, null=True, choices=STATUS)
+    status = models.CharField(max_length=25, null=True, choices=STATUS)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
+
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total_orders = orders.count()
+    total_customers = customers.count()
+    pending_orders = orders.filter(status='Pending').count()
+    delivered_orders = orders.filter(status='Delivered').count()
+    context = {
+        'orders': orders,
+        'customers': customers,
+        'total_orders': total_orders,
+        'total_customers': total_customers,
+        'pending_orders': pending_orders,
+        'delivered_orders': delivered_orders
+    }
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products': products})
 
 
 def customers(request):
